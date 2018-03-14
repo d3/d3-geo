48e8a215a3f0e4e34eaa36c35baf0d8bcfe990b6
Philippe Rivière <fil@rezo.net>
2018-03-14 16:48:16 +0100

    fix .angle(30).angle() == 30 test (conversion to radians then degrees generates drift); do not require angle to be -180<x<=180

--- test/projection/angle-test.js
+++ test/projection/angle-test.js
@@ -7,13 +7,10 @@ tape("test angles", function(test) {
   test.equal(projection([10,0])[1], 0);
   projection.angle(30);
   test.deepEqual(projection([0,0]), [0,0]);
-  test.equal(projection.angle(), 30);
+  test.inDelta(projection.angle(), 30, 1e-12);
   test.inDelta(projection([10,0])[1], -0.08816349, 1e-6);
   test.inDelta(projection.invert(projection([12.3,5]))[0], 12.3, 1e-6);
   projection.angle(180);
   test.inDelta(projection([10,0])[1], 0, 1e-12);
-  test.equal(projection.angle(), -180);
-  projection.angle(5702);
-  test.equal(projection.angle(), -58);
   test.end();
 });
