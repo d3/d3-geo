a102edcfd703617e13120c87a4952e4e4a002e88
Mike Bostock <mbostock@gmail.com>
2022-12-18 09:23:47 -0800

    restore direct append

--- src/path/string.js
+++ src/path/string.js
@@ -1,9 +1,9 @@
 // Simple caching for constant-radius points.
-let cacheDigits, cacheTemplate, cacheRadius, cacheCircle;
+let cacheDigits, cacheAppend, cacheRadius, cacheCircle;
 
 export default class PathString {
   constructor(digits) {
-    this._template = digits == null ? template : templateFixed(digits = +digits);
+    this._append = digits == null ? append : appendFixed(digits = +digits);
     this._radius = 4.5;
     this._ = "";
   }
@@ -27,20 +27,22 @@ export default class PathString {
   point(x, y) {
     switch (this._point) {
       case 0: {
-        this._ += this._template`M${x},${y}`;
+        this._append`M${x},${y}`;
         this._point = 1;
         break;
       }
       case 1: {
-        this._ += this._template`L${x},${y}`;
+        this._append`L${x},${y}`;
         break;
       }
       default: {
-        this._ += this._template`M${x},${y}`;
-        if (this._template !== cacheTemplate || this._radius !== cacheRadius) {
+        this._append`M${x},${y}`;
+        if (this._append !== cacheAppend || this._radius !== cacheRadius) {
           const r = cacheRadius = this._radius;
-          cacheTemplate = this._template;
-          cacheCircle = this._template`m0,${r}a${r},${r} 0 1,1 0,${-2 * r}a${r},${r} 0 1,1 0,${2 * r}z`;
+          const p = new PathString(this._digits);
+          p._append`m0,${r}a${r},${r} 0 1,1 0,${-2 * r}a${r},${r} 0 1,1 0,${2 * r}z`;
+          cacheAppend = this._append;
+          cacheCircle = p._;
         }
         this._ += cacheCircle;
         break;
@@ -54,25 +56,25 @@ export default class PathString {
   }
 }
 
-function template(strings) {
-  let i = 1, string = strings[0];
+function append(strings) {
+  let i = 1;
+  this._ += strings[0];
   for (const j = strings.length; i < j; ++i) {
-    string += arguments[i] + strings[i];
+    this._ += arguments[i] + strings[i];
   }
-  return string;
 }
 
-function templateFixed(digits) {
+function appendFixed(digits) {
   if (digits !== cacheDigits) {
     (0).toFixed(digits); // validate digits
     cacheDigits = digits;
-    cacheTemplate = function template(strings) {
-      let i = 1, string = strings[0];
+    cacheAppend = function append(strings) {
+      let i = 1;
+      this._ += strings[0];
       for (const j = strings.length; i < j; ++i) {
-        string += +arguments[i].toFixed(digits) + strings[i];
+        this._ += +arguments[i].toFixed(digits) + strings[i];
       }
-      return string;
     };
   }
-  return cacheTemplate;
+  return cacheAppend;
 }
